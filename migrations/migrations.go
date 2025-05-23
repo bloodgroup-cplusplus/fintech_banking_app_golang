d86cd668e78c631b53be30c1cc8ca5ae90f4a7ff
bloodgroupcplusplus <tucater10896601@protonmail.com>
2025-05-23 01:40:56 -0400

    adding more to migrations

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -8,4 +8,43 @@ import (
 
 
 
+type User struct {
+	gorm.Model
+	Username string
+	Email string
+	Password string
+}
+
+
+type Account struct {
+	gorm.Model
+	Type string
+	Name string
+	Balance uint
+	UserID uint
+}
+
+
+func connectDB() *grom.DB {
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=postgres dbname=bankapp password=postgres sslmode=disabled")
+
+	helpers.HanldeErr(err)
+	return db
+}
+
+func createAccounts() {
+	db := connectDB()
+	user := [2] User {
+	{Username: "Martin", Email:"martin@martin.com"}
+	{Username:"Chad", Email:"chad@chad.com"},
+	}
+
+	for i :=0 ; i<len(users); i++ {
+		generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
+		user := User{Username: users[i].Username, users[i].Email, Password:genratedPassword}
+
+	
+
+
+
 
